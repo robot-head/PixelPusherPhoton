850296decb4d46686fa37671f73e9790e40535ba
robot-head <matthew.d.stone@gmail.com>
2014-05-14 09:25:23 -0700

    Add PixelPusher integration classes

---
+++ src/com/heroicrobot/pixelpusherphoton/PixelPusherPhotonApplication.java
@@ -0,0 +1,40 @@
+package com.heroicrobot.pixelpusherphoton;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import com.heroicrobot.dropbit.devices.pixelpusher.PixelPusher;
+import com.heroicrobot.dropbit.registry.DeviceRegistry;
+
+import android.app.Application;
+import android.util.Log;
+
+public class PixelPusherPhotonApplication extends Application {
+
+	public static final String TAG = "PixelPusherPhoton";
+
+	class pusherObserver implements Observer {
+
+		@Override
+		public void update(Observable registry, Object updatedDevice) {
+			if (updatedDevice != null) {
+				PixelPusher device = (PixelPusher)updatedDevice;
+				Log.i(TAG, "Updated pusher: " + device.toString());
+			}
+		}
+
+	}
+
+	DeviceRegistry registry;
+	pusherObserver observer;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		registry = new DeviceRegistry();
+		observer = new pusherObserver();
+		registry.addObserver(observer);
+		registry.setAntiLog(true);
+	}
+
+}
---
+++ src/com/heroicrobot/pixelpusherphoton/tasks/DiscoveryTask.java
@@ -0,0 +1,17 @@
+package com.heroicrobot.pixelpusherphoton.tasks;
+
+import java.util.List;
+
+import android.os.AsyncTask;
+
+import com.heroicrobot.dropbit.devices.pixelpusher.*;
+
+public class DiscoveryTask extends AsyncTask<Void, Integer, List<PixelPusher>> {
+
+	@Override
+	protected List<PixelPusher> doInBackground(Void... arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
