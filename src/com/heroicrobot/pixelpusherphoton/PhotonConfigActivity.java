b73ca98c07452af883a40f1b6c1cb99a1134eec4
robot-head <evil@heroicrobot.com>
2014-05-30 14:06:57 -0700

    More config activity hacking

--- src/com/heroicrobot/pixelpusherphoton/PhotonConfigActivity.java
+++ src/com/heroicrobot/pixelpusherphoton/PhotonConfigActivity.java
@@ -1,9 +1,16 @@
 package com.heroicrobot.pixelpusherphoton;
 
+import com.heroicrobot.pixelpusherphoton.RegistryService.LocalBinder;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,12 +22,16 @@ public class PhotonConfigActivity extends ActionBarActivity {
 	
 	private static final String PIXEL_PUSHER_MAC_ADDR_KEY = "pixel_pusher_mac_addr";
 	private String mPusherMac;
+	protected RegistryService myService;
+	protected boolean isBound;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_photon_config);
-
+		Intent intent = new Intent(this, RegistryService.class);
+		bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
+		
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
@@ -29,6 +40,8 @@ public class PhotonConfigActivity extends ActionBarActivity {
 		if (extras != null) {
 			mPusherMac = extras.getString(PIXEL_PUSHER_MAC_ADDR_KEY);
 		}
+		
+		
 	}
 
 	@Override
@@ -67,5 +80,19 @@ public class PhotonConfigActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
+	
+	private ServiceConnection myConnection = new ServiceConnection() {
+
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			LocalBinder binder = (LocalBinder) service;
+			myService = binder.getService();
+			isBound = true;
+		}
+
+		public void onServiceDisconnected(ComponentName arg0) {
+			isBound = false;
+		}
+
+	};
 
 }
